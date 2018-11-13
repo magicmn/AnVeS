ee8d59d22b803727930f96d011c89051d42e823c
magicmn <julianklein1@gmail.com>
2018-11-13 13:48:16 +0100

    VertragDAO gerettet

--- View/src/main/java/de/anves/view/controller/KundenAnlegenController.java
+++ View/src/main/java/de/anves/view/controller/KundenAnlegenController.java
@@ -1,30 +1,23 @@
 package de.anves.view.controller;
 
-import de.anves.Kunde;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class KundenAnlegenController {
 
-
+    @GetMapping("/KundeAnlegen")
+    public String kundenAnlegenForm(Model model) {
+        //model.addAttribute("KundeAnlegen", new Kunde());
+        return "KundeAnlegen";
+    }
 
 /*
-    @GetMapping("/KundenAnlegen")
-    public String kundenAnlegenForm(Model model) {
-        model.addAttribute("KundenAnlegen", new Kunde());
-        return "KundenAnlegen";
-    }
-*/
-
-
-    @PostMapping("/KundenAnlegen")
+    @PostMapping("/KundeAnlegen")
     public String kundenAnlegenSubmit(@ModelAttribute Kunde kunde) {
         return "result";
-    }
+    }*/
 
 }
