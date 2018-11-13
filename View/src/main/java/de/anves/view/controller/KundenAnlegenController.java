035a44859c472074fe44ade9c56e5e615d34c844
AnykeyXD <toni-oliver.kowalski@gmx.de>
2018-11-13 13:08:56 +0100

    kundenanlegencontroller

--- View/src/main/java/de/anves/view/controller/KundenAnlegenController.java
+++ View/src/main/java/de/anves/view/controller/KundenAnlegenController.java
@@ -1,16 +1,30 @@
 package de.anves.view.controller;
 
+import de.anves.Kunde;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class KundenAnlegenController {
 
-    @GetMapping("/")
-    public String kundenAnlegen(Model model) {
-        /*model.addAttribute("KundenAnlegen", new Kunde());*/
-        return "kundeAnlegen";
+
+
+/*
+    @GetMapping("/KundenAnlegen")
+    public String kundenAnlegenForm(Model model) {
+        model.addAttribute("KundenAnlegen", new Kunde());
+        return "KundenAnlegen";
+    }
+*/
+
+
+    @PostMapping("/KundenAnlegen")
+    public String kundenAnlegenSubmit(@ModelAttribute Kunde kunde) {
+        return "result";
     }
 
 }
