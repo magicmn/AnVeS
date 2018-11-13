e16d8a0e1c2d60edfd0a48459dedee624633cf48
magicmn <julianklein1@gmail.com>
2018-11-13 13:49:32 +0100

    Ordner struktur wiederhergestellt.

--- View/src/main/java/de/anves/view/controller/KundenSuchenController.java
+++ View/src/main/java/de/anves/view/controller/KundenSuchenController.java
@@ -1,11 +1,12 @@
-package de.anves.view.Application;
+package de.anves.view.controller;
 
 import de.anves.Kunde;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class KundenSuchenController {
 
