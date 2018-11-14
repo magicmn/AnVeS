c5a8bca72b77b8b2c7b822773f4d345af1fbba59
Konstantin <>
2018-11-14 09:14:45 +0100

    AnhängerDAO v.03 tested
    Signed-off-by: Konstantin <>

---
+++ View/src/main/java/de/anves/view/controller/VertragSuchenAuswaehlenController.java
@@ -0,0 +1,29 @@
+package de.anves.view.controller;
+
+
+import de.anves.Vertrag;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+
+@Controller
+public class VertragSuchenAuswaehlenController {
+
+
+    @GetMapping("/VertragAuswaehlen")
+    public String vertragAuswaehlen(@RequestParam(required = false) ArrayList<Vertrag> vertragsliste, Model model) {
+        model.addAttribute("vertragsliste", vertragsliste);
+
+        return "VertragAuswaehlen";
+    }
+    @GetMapping("/VertragSuchen")
+    public String vertragSuchen( Model model) {
+
+        return "VertragSuchen";
+    }
+}
