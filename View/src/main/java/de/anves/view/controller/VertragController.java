954b6f0a2fc309a3d728e7de20cc8554b56a6a2a
Konstantin <>
2018-11-14 13:15:54 +0100

    VertragController mit beispiel daten
    Signed-off-by: Konstantin <>

--- View/src/main/java/de/anves/view/controller/VertragController.java
+++ View/src/main/java/de/anves/view/controller/VertragController.java
@@ -1,6 +1,9 @@
 package de.anves.view.controller;
 
 
+import de.anves.Kunde;
+import de.anves.Mitarbeiter;
+import de.anves.Reservierung;
 import de.anves.Vertrag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 @Controller
@@ -17,6 +21,19 @@ public class VertragController {
 
     @GetMapping("/VertragAuswaehlen")
     public String vertragAuswaehlen(@RequestParam(required = false) ArrayList<Vertrag> vertragsliste, Model model) {
+        vertragsliste = new ArrayList<>();
+        Vertrag vertrag = new Vertrag();
+        vertrag.setId(123);
+        vertrag.setReservierung(new Reservierung());
+        vertrag.getReservierung().setKunde(new Kunde());
+        vertrag.getReservierung().getKunde().setId(123456789);
+        vertrag.getReservierung().getKunde().setNachname("Hans");
+        vertrag.getReservierung().getKunde().setVorname("Peter");
+
+        vertrag.setRueckgabe(new Date(100000000));
+        vertrag.setUebergabe(new Date(100000000));
+        vertrag.setRueckgabeMitarbeiter(new Mitarbeiter());
+        vertragsliste.add(vertrag);
         model.addAttribute("vertragsliste", vertragsliste);
 
         return "VertragAuswaehlen";
