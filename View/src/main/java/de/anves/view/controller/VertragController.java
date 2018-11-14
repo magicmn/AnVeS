bb3aa0a26c6d16799dd7e36b3170bb35f9ae7f47
Konstantin <>
2018-11-14 13:59:08 +0100

    Richtige views, bäh
    Signed-off-by: Konstantin <>

--- View/src/main/java/de/anves/view/controller/VertragController.java
+++ View/src/main/java/de/anves/view/controller/VertragController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -39,8 +40,57 @@ public class VertragController {
         return "VertragAuswaehlen";
     }
     @GetMapping("/VertragSuchen")
-    public String vertragSuchen( Model model) {
+    public String getVertragSuchen(Model model) {
+        model.addAttribute("suchform",new Suchform());
+
+        return "suchform";
+    }
+    @GetMapping("/VertragSuchen")
+    public String postVertragSuchen(@ModelAttribute Suchform form, Model model) {
+        model.addAttribute("suchform",form);
 
         return "VertragSuchen";
     }
+    public class Suchform
+    {
+        private long vertragID;
+        private long kundenID;
+
+        public long getVertragID() {
+            return vertragID;
+        }
+
+        public void setVertragID(long vertragID) {
+            this.vertragID = vertragID;
+        }
+
+        public long getKundenID() {
+            return kundenID;
+        }
+
+        public void setKundenID(long kundenID) {
+            this.kundenID = kundenID;
+        }
+
+        public Date getVon() {
+            return von;
+        }
+
+        public void setVon(Date von) {
+            this.von = von;
+        }
+
+        public Date getBis() {
+            return bis;
+        }
+
+        public void setBis(Date bis) {
+            this.bis = bis;
+        }
+
+        private Date von;
+        private Date bis;
+
+    }
+
 }
