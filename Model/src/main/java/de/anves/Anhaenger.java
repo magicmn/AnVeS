f5158f96e81f58ad665f4261795dcc2bd810111d
magicmn <julianklein1@gmail.com>
2018-11-15 12:08:39 +0100

    Models für Anhänger optimiert.

--- Model/src/main/java/de/anves/Anhaenger.java
+++ Model/src/main/java/de/anves/Anhaenger.java
@@ -1,5 +1,6 @@
 package de.anves;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -11,42 +12,74 @@ public class Anhaenger extends DBManaged {
     private Date naechsteHU;
     private List<Schadensbericht> schadensBerichte;
 
-//Getter and Setter-------------------------------
+    /**
+     * Leerer Konstruktor = Daten nachträglich setzen.
+     */
+    public Anhaenger() {
+    }
+
+    /**
+     * Konstruktor der erzwingt alle Felder zu setzen um Fehler zu vermeiden.
+     */
+    public Anhaenger(long id, AnhaengerTypEnum anhaengerTyp, String kennzeichen, Date naechsteHU, List<Schadensbericht> schadensBerichte) {
+        this.id = id;
+        this.anhaengerTyp = anhaengerTyp;
+        this.kennzeichen = kennzeichen;
+        this.naechsteHU = naechsteHU;
+        this.schadensBerichte = schadensBerichte;
+    }
 
     public long getId() {
         return id;
     }
+
     public Anhaenger setId(long id) {
         this.id = id;
         return this;
     }
+
     public AnhaengerTypEnum getAnhaengerTyp() {
         return anhaengerTyp;
     }
+
     public Anhaenger setAnhaengerTyp(AnhaengerTypEnum anhaengerTyp) {
         this.anhaengerTyp = anhaengerTyp;
         return this;
     }
+
     public String getKennzeichen() {
         return kennzeichen;
     }
+
     public Anhaenger setKennzeichen(String kennzeichen) {
         this.kennzeichen = kennzeichen;
         return this;
     }
+
     public Date getNaechsteHU() {
         return naechsteHU;
     }
+
     public Anhaenger setNaechsteHU(Date naechsteHU) {
         this.naechsteHU = naechsteHU;
         return this;
     }
+
     public List<Schadensbericht> getSchadensBerichte() {
         return schadensBerichte;
     }
+
     public Anhaenger setSchadensBerichte(List<Schadensbericht> schadensBerichte) {
         this.schadensBerichte = schadensBerichte;
         return this;
     }
 
+    public String getSchadensBerichteString() {
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sdt = new SimpleDateFormat("dd.MM.yyyy");
+        for (Schadensbericht schadensBericht : schadensBerichte) {
+            sb.append("Am ").append(sdt.format(schadensBericht.getDatum())).append(schadensBericht.getBeschreibung());
+        }
+        return sb.toString();
+    }
 }
--- Model/src/main/java/de/anves/AnhaengerTypEnum.java
+++ Model/src/main/java/de/anves/AnhaengerTypEnum.java
@@ -1,6 +1,11 @@
 package de.anves;
 
 public enum AnhaengerTypEnum {
-    BELIEBIGER, EINACHSER, TANDEMACHSER, KOFFERANHAENGER, MOTORRADANHAENGER
+    EINACHSER(1), TANDEMACHSER(2), KOFFERANHAENGER(3), MOTORRADANHAENGER(4);
 
+    public final long id;
+
+    AnhaengerTypEnum(long id) {
+        this.id = id;
+    }
 }
